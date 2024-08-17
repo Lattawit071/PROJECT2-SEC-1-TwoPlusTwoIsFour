a0ae19195b3234bc4373f8c5f9ae7e1ec0a818f1
Fujipp <fujipp.official@gmail.com>
2024-08-17 20:56:05 +0700

    First page
    Lobby

--- Fishing/src/App.vue
+++ Fishing/src/App.vue
@@ -1,47 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <!-- <div style="display: flex; align-items: center; justify-content: center; min-height: 100vh; background-color: #f7fafc; position: relative;"> -->
+    <div class="main-container">
+    <!-- Top Left Button -->
+    <div style="position: absolute; top: 1rem; left: 1rem;">
+      <img src="./components/icons/Setting.png" alt="Settings" style="width: 15rem; height: 15rem; cursor: pointer; object-fit: contain;">
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <!-- Top Right Button -->
+    <div style="position: absolute; top: 1rem; right: 1rem;">
+      <img src="./components/icons/Question.png" alt="Help" style="width: 15rem; height: 15rem; cursor: pointer; object-fit: contain;">
+    </div>
+    <!-- Center Content -->
+    <div style="text-align: center;">
+      <div style="font-size: 10rem; font-family: 'YourHandwrittenFont', sans-serif; margin-bottom: 10rem;">
+        fish for life
+      </div>
+      <div style="position: relative; display: inline-block;">
+        <img src="./components/icons/Button.png" alt="Play" style="width: 40rem; height: 15rem; margin: 0 auto; cursor: pointer; object-fit: contain;">
+        <span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 6rem; color: white; font-family: 'YourHandwrittenFont', sans-serif;">
+          PLAY
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
+<script>
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.main-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-image: url('./components/image/Background.png'); /* เปลี่ยน path ของรูปตามที่ต้องการ */
+  background-size: cover; /* ปรับขนาดให้รูปภาพครอบคลุมทั้ง background */
+  background-position: center; /* ปรับให้รูปอยู่ตรงกลาง */
+  position: relative;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+@font-face {
+  font-family: 'YourHandwrittenFont';
+  src: url('/path/to/your/font-file.woff2') format('woff2');
 }
 </style>
